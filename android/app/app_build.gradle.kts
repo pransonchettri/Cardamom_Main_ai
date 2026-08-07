9613939ef3797642d95624a0e57ee9f639eb28d7
pransonchettri <pransonchettri7@gmail.com>
2026-08-07 12:50:52 +0530

    CardaCare

--- android/app/app_build.gradle.kts
+++ android/app/app_build.gradle.kts
@@ -43,32 +43,3 @@ kotlin {
 flutter {
     source = "../.."
 }
-plugins {
-  id("com.android.application")
-
-  // Add the Google services Gradle plugin
-  id("com.google.gms.google-services")
-
-  ...
-}
-
-dependencies {
-  // Import the Firebase BoM
-  implementation(platform("com.google.firebase:firebase-bom:34.17.0"))
-
-
-  // TODO: Add the dependencies for Firebase products you want to use
-  // When using the BoM, don't specify versions in Firebase dependencies
-  implementation("com.google.firebase:firebase-analytics")
-
-
-  // Add the dependencies for any other desired Firebase products
-  // https://firebase.google.com/docs/android/setup#available-libraries
-}
-plugins {
-  // ...
-
-  // Add the dependency for the Google services Gradle plugin
-  id("com.google.gms.google-services") version "4.5.0" apply false
-
-}
--- android/app/src/main/kotlin/com/example/plant_ai/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.plant_ai
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
